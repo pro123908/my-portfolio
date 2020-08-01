600bef08cf08985c6e73fcd2e17aef1bf3a6af8e
Bilal Ahmad <pro123908@gmail.com>
2020-08-01 20:50:05 +0500

    Added two consecutive timelines

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect } from "react";
 import "./App.scss";
 
-import { TweenMax, Power3 } from "gsap";
+import { TweenMax, Power3, TimelineMax } from "gsap";
+import Counter from "./counter";
 
 function App() {
   let introText = useRef();
@@ -9,38 +10,75 @@ function App() {
   let introSelf2 = useRef();
   let introSelf3 = useRef();
 
+  let testTimeline = new TimelineMax();
+
+  let introTimeline = new TimelineMax({ defaults: { duration: 1 } });
+
   // console.log(introText);
 
   useEffect(() => {
     console.log(introSelf2);
     // TweenMax.to(introText.current, 1, { });
-    TweenMax.from(introText.current, 1, {
-      x: "-100%",
-      ease: Power3,
-      scale: 1.1,
-    });
 
-    TweenMax.from(introSelf1.current, 1, {
+    introTimeline
+      .from(introText.current, 1, {
+        x: "-100%",
+        ease: Power3,
+        scale: 1.1,
+      })
+      .from(introSelf1.current, 1, {
+        x: "-100vw",
+        ease: Power3,
+      })
+      .from(introSelf2.current, 1, {
+        y: "100vh",
+        ease: Power3,
+        // delay: 1.5,
+        scale: 2,
+      });
+
+    testTimeline.from(introSelf3.current, 1, {
       x: "-100vw",
       ease: Power3,
-      delay: 1,
+      // delay: 3,
     });
 
-    TweenMax.from(introSelf2.current, 1, {
-      y: "100vh",
-      ease: Power3,
-      delay: 1.5,
-      scale: 2,
-    });
+    testTimeline.delay(introTimeline.duration() + 4);
 
-    TweenMax.from(introSelf3.current, 1, {
-      x: "-100vw",
-      ease: Power3,
-      delay: 3,
-    });
+    setTimeout(() => {
+      introTimeline.timeScale(3);
+      introTimeline.reverse();
+    }, (introTimeline.duration() + 3) * 1000);
+
+    // TweenMax.from(introText.current, 1, {
+    //   x: "-100%",
+    //   ease: Power3,
+    //   scale: 1.1,
+    // });
+
+    // TweenMax.from(introSelf1.current, 1, {
+    //   x: "-100vw",
+    //   ease: Power3,
+    //   delay: 1,
+    // });
+
+    // TweenMax.from(introSelf2.current, 1, {
+    //   y: "100vh",
+    //   ease: Power3,
+    //   delay: 1.5,
+    //   scale: 2,
+    // });
+
+    // TweenMax.from(introSelf3.current, 1, {
+    //   x: "-100vw",
+    //   ease: Power3,
+    //   delay: 3,
+    // });
   }, []);
   return (
-    <div className="App">
+    <div className="app">
+      <Counter time={6} text={"Reversing in "} />
+
       <div className="intro-text" ref={introText}>
         Hello World, Welcome to my portfolio
       </div>
---
+++ src/counter.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const Counter = ({ time }) => {
+  const [counter, setCounter] = useState(time);
+  const [text, setText] = useState("Reversing in");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((counter) => {
+        console.log(counter);
+        if (counter === 0) {
+          setText("Reversed");
+          clearInterval(interval);
+        } else return counter - 1;
+      });
+
+      //   setCounter((counter) => {
+      //     if (counter === 0) {
+      //       clearInterval(interval);
+      //     } else return counter - 1;
+      //   });
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return (
+    <div className="counter">
+      {text} {counter}
+    </div>
+  );
+};
+
+export default Counter;
